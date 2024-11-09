5395f8a0fcbcf93a485da3f8b7e5e5c67cbbf4f5
Rus <rus.zakirov@triosstudent.com>
2024-11-09 18:21:41 -0500

    Added Todos component

--- to-do-app/src/app/components/todos/todos.component.ts
+++ to-do-app/src/app/components/todos/todos.component.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Todo } from './../../models/todo'
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
 export class TodosComponent {
 
-  todos = [];
+  todos:Todo[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    
+    this.todos = [
+      {
+        content: 'First todo',
+        completed: false
+      },
+      {
+        content: 'Second tod',
+        completed: false
+      }
+    ]
   }
 }
